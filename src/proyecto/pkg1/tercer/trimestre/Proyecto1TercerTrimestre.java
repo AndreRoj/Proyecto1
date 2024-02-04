f03ff809fdc0df975cca0cdc95c17b295707e461
luciano2777 <mader1930@gmail.com>
2024-02-03 22:18:40 -0400

    Prueba

---
+++ src/proyecto/pkg1/tercer/trimestre/Proyecto1TercerTrimestre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg1.tercer.trimestre;
+
+/**
+ *
+ * @author luciano
+ */
+public class Proyecto1TercerTrimestre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
