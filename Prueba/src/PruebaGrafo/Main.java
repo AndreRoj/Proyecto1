a10f6c092c0696ada1b533f6d174bf96645057f5
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2024-02-16 21:27:58 -0400

    Add files via upload

---
+++ Prueba/src/PruebaGrafo/Main.java
@@ -0,0 +1,30 @@
+
+package PruebaGrafo;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Graph graph = new SingleGraph("tutorial1");
+       //graph.addNode("A" );
+       //graph.addNode("B" );
+       //graph.addNode("C" );
+       //graph.addEdge("AB", "A", "B");
+       //graph.addEdge("BC", "B", "C");
+       //graph.addEdge("CA", "C", "A");
+     graph.setStrict(false);
+     graph.setAutoCreate( true );
+     graph.addEdge( "AB", "A", "B" );
+     graph.addEdge( "BC", "B", "C" );
+     for (Node node : graph) {
+        node.setAttribute("ui.label", node.getId());
+    }
+     for (Edge e : graph.getEachEdge()) {
+        e.addAttribute("ui.label", 1);
+          }
+     graph.display();
+               
+    }
+    
+}
