c694b32ab827babad8203dd23f8342e293e00b5c
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2024-02-18 01:05:22 -0400

    Interfaces
    Ventanas del 1 al 7

--- Proyecto1EDD/src/Clases/Main.java
+++ Proyecto1EDD/src/Clases/Main.java
@@ -1,6 +1,8 @@
 
 package Clases;
 
+import Interfaces.Ventana1;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +16,10 @@ public class Main {
         for (int i = 0; i < copia.getMaximo(); i++) {
             System.out.println(copia.buscar(1)[i]);
         }
+        Ventana1 ventana1 = new Ventana1();
+        ventana1.setVisible(true);
+        
     }
+    
     
 }
