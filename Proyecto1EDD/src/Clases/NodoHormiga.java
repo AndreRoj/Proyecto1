27823fc3862d2baca50a5e7b1e20f629cfd88a8d
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2024-02-18 23:34:57 -0400

    ListaHormigas y NodoHormigas

---
+++ Proyecto1EDD/src/Clases/NodoHormiga.java
@@ -0,0 +1,30 @@
+
+package Clases;
+
+
+public class NodoHormiga {
+    private Hormigas element;
+    private NodoHormiga next;
+
+    public NodoHormiga(Hormigas element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    public Hormigas getElement() {
+        return element;
+    }
+
+    public void setElement(Hormigas element) {
+        this.element = element;
+    }
+
+    public NodoHormiga getNext() {
+        return next;
+    }
+
+    public void setNext(NodoHormiga next) {
+        this.next = next;
+    }
+    
+}
